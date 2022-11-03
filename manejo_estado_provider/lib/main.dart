5808cc10c28d49da0b343b08918ad2b7625f83f3
Gabriel Navarro <alt64b@gmail.com>
2022-11-02 22:38:42 -0300

    provider text

--- manejo_estado_provider/lib/main.dart
+++ manejo_estado_provider/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => HeroeInfo()),
+        ChangeNotifierProvider(create: (context) => HeroeInfo()), //lista
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
--- manejo_estado_provider/lib/widget/super_text.dart
+++ manejo_estado_provider/lib/widget/super_text.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/heroinfo.dart';
 
 class SuperText extends StatelessWidget {
   const SuperText({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final heroesinfo = Provider.of<HeroeInfo>(context);
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("Super heroes"),
+        Text(heroesinfo.getHeroe),
       ],
     );
   }
